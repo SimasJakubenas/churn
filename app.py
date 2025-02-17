b9bd07b8b65819b5567334293ddaab00f9d95045
sjakubenas <sjakubenas@gmail.com>
2025-02-17 07:54:20 +0200

    Add ml churn streamlit page

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 from app_pages.page1_summary import page_summary
 from app_pages.page2_study import page_study
+from app_pages.page3_ml_chern import page_churn
 
 
 app = MultiPage(app_name= "Telco Churnometer")
 
 app.app_page("Project Summary", 'house', page_summary)
 app.app_page("Study", 'graph-up-arrow', page_study)
+app.app_page("ML Churn Prediction", 'arrow-repeat', page_churn)
 
 app.run()
